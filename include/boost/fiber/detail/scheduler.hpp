501840f07740fe5d879268bae2d95c993a7b8d50
Oliver Kowalke <oliver.kowalke@gmx.de>
2012-12-13 19:19:05 +0100

    instance pointer not function-local

--- include/boost/fiber/detail/scheduler.hpp
+++ include/boost/fiber/detail/scheduler.hpp
@@ -6,6 +6,10 @@
 #ifndef BOOST_FIBERS_DETAIL_SCHEDULER_H
 #define BOOST_FIBERS_DETAIL_SCHEDULER_H
 
+#if defined(__APPLE__) && defined(BOOST_HAS_PTHREADS)
+#include <pthread.h>                // pthread_key_create, pthread_[gs]etspecific
+#endif
+
 #include <cstddef>
 #include <deque>
 #include <vector>
@@ -35,6 +39,57 @@
 namespace boost {
 namespace fibers {
 namespace detail {
+
+class scheduler;
+
+// thread_local_ptr was a contribution from
+// Nat Goodspeed
+#if defined(__APPLE__) && defined(BOOST_HAS_PTHREADS)
+class thread_local_ptr : private noncopyable
+{
+private:
+    struct dummy
+    { void nonnull() {} };
+
+    typedef void ( dummy::*safe_bool)();
+
+    ::pthread_key_t     key_;
+
+public:
+    thread_local_ptr() BOOST_NOEXCEPT
+    { BOOST_ASSERT( ! ::pthread_key_create( & key_, 0) ); }
+
+    scheduler * get() const BOOST_NOEXCEPT
+    { return static_cast< scheduler * >( ::pthread_getspecific( key_) ); }
+
+    thread_local_ptr & operator=( scheduler * ptr) BOOST_NOEXCEPT
+    {
+        ::pthread_setspecific( key_, ptr);
+        return * this;
+    }
+
+    scheduler & operator*() const BOOST_NOEXCEPT
+    { return * get(); }
+
+    scheduler * operator->() const BOOST_NOEXCEPT
+    { return get(); }
+
+    operator scheduler * () const BOOST_NOEXCEPT
+    { return get(); }
+
+    operator safe_bool() const BOOST_NOEXCEPT
+    { return get() ? 0 : & dummy::nonnull; }
+
+    bool operator!() const BOOST_NOEXCEPT
+    { return ! get(); }
+
+    bool operator==( thread_local_ptr const& other) BOOST_NOEXCEPT
+    { return this->get() == other.get(); }
+
+    bool operator!=( thread_local_ptr const& other) BOOST_NOEXCEPT
+    { return ! ( * this == other); }
+};
+#endif
 
 class BOOST_FIBERS_DECL scheduler : private noncopyable
 {
@@ -77,6 +132,15 @@ private:
     typedef wqueue_t::index< f_tag_t >::type    f_idx_t;
     typedef wqueue_t::index< tp_tag_t >::type   tp_idx_t;
     typedef std::deque< fiber_base::ptr_t >     rqueue_t;
+
+#if defined(_MSC_VER) || defined(__BORLANDC__) || defined(__DMC__) || \
+    (defined(__ICC) && defined(BOOST_WINDOWS))
+    static __declspec(thread) scheduler     *   instance_;
+#elif defined(BOOST_MAC_PTHREADS)
+    static thread_local_ptr                     instance_;
+#else
+    static __thread scheduler               *   instance_;
+#endif
 
     fiber_base::ptr_t       active_fiber_;
     rqueue_t                rqueue_;
