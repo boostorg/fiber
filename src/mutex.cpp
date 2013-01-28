95f989f9ac2977452f85be7e37d74ed275549a2e
Oliver Kowalke <oliver.kowalke@gmx.de>
2013-01-28 18:05:33 +0100

    do yield if fiber exec run() otherwise in mutex

--- src/mutex.cpp
+++ src/mutex.cpp
@@ -34,12 +34,14 @@ mutex::lock()
 {
     while ( LOCKED == state_.exchange( LOCKED, memory_order_seq_cst) )
     {
-        BOOST_ASSERT( this_fiber::is_fiberized() );
-
-        unique_lock< detail::spinlock > lk( waiting_mtx_);
-        waiting_.push_back(
-                detail::scheduler::instance().active() );
-        detail::scheduler::instance().wait( lk);
+        if ( this_fiber::is_fiberized() )
+        {
+            unique_lock< detail::spinlock > lk( waiting_mtx_);
+            waiting_.push_back(
+                    detail::scheduler::instance().active() );
+            detail::scheduler::instance().wait( lk);
+        }
+        else run();
     }
 }
 
