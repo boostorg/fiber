232c9118d53917d3133c20d2752c5b3e25afb23d
Oliver Kowalke <oliver.kowalke@gmx.de>
2013-01-31 17:38:42 +0100

    add comment regarding to derived code

--- src/condition.cpp
+++ src/condition.cpp
@@ -3,6 +3,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
+//
+//  based on boost::interprocess::sync::interprocess_condition
 
 #define BOOST_FIBERS_SOURCE
 
