a2611f032ab9b845cbd7e592bfe4a146c67dea20
olk <oliver.kowalke@gmail.com>
2014-01-07 07:48:53 +0100

    change copy right in example/future_mt.cpp

--- examples/futures_mt.cpp
+++ examples/futures_mt.cpp
@@ -1,4 +1,4 @@
-//  (C) Copyright 2008-10 Anthony Williams 
+//  (C) Copyright 2013 Oliver Kowalke
 //
 //  Distributed under the Boost Software License, Version 1.0. (See
 //  accompanying file LICENSE_1_0.txt or copy at
