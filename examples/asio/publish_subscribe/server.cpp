552c175be1757ff47f30227959ef2f9726532821
olk <oliver.kowalke@gmail.com>
2014-01-07 08:46:26 +0100

    add some comments to publish-subscibe example

--- examples/asio/publish_subscribe/server.cpp
+++ examples/asio/publish_subscribe/server.cpp
@@ -1,9 +1,8 @@
 //
-// echo_server2.cpp
+// server.cpp
 // ~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//               2013      Oliver Kowalke
+// Copyright (c) 2013 Oliver Kowalke
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -30,14 +29,18 @@ const int max_length = 1024;
 
 class subscriber_session;
 
+// a channel has n subscribers (subscriptions)
+// this class holds a list of subcribers for one channel
 class subscriptions
 {
 public:
     ~subscriptions();
 
+    // subscribe to this channel
     void subscribe( boost::shared_ptr< subscriber_session > const& s)
     { subscribers_.push_back( s); }
 
+    // unsubscribe from this channel
     void unsubscribe( boost::shared_ptr< subscriber_session > const& s)
     {
         std::vector< boost::shared_ptr< subscriber_session > >::iterator e = subscribers_.end();
@@ -54,13 +57,15 @@ public:
         }
     }
 
+    // publish a message, e.g. push this message to all subscribers
     void publish( std::string const& msg);
 
 private:
+    // list of subscribers
     std::vector< boost::shared_ptr< subscriber_session > >  subscribers_;
 };
 
-
+// a class to register channels and to subsribe clients to this channels
 class registry : private boost::noncopyable
 {
 private:
@@ -104,31 +109,35 @@ private:
     }
 
 public:
+    // add a channel to registry
     void register_channel( std::string const& channel)
     {
         boost::fibers::mutex::scoped_lock lk( mtx_);
         register_channel_( channel);
     }
 
+    // remove a channel from registry
     void unregister_channel( std::string const& channel)
     {
         boost::fibers::mutex::scoped_lock lk( mtx_);
         unregister_channel_( channel);
     }
 
+    // subscribe to a channel
     void subscribe( std::string const& channel, boost::shared_ptr< subscriber_session > s)
     {
         boost::fibers::mutex::scoped_lock lk( mtx_);
         subscribe_( channel, s);
     }
 
+    // unsubscribe from a channel
     void unsubscribe( std::string const& channel, boost::shared_ptr< subscriber_session > s)
     {
         boost::fibers::mutex::scoped_lock lk( mtx_);
         unsubscribe_( channel, s);
     }
 
-
+    // publish a message to all subscribers registerd to the channel
     void publish( std::string const& channel, std::string const& msg)
     {
         boost::fibers::mutex::scoped_lock lk( mtx_);
@@ -136,7 +145,7 @@ public:
     }
 };
 
-
+// a subscriber subscribes to a given channel in order to receive messages published on this channel
 class subscriber_session : public boost::enable_shared_from_this< subscriber_session >
 {
 public:
@@ -148,31 +157,47 @@ public:
     tcp::socket& socket()
     { return socket_; }
 
+    // this function is executed inside the fiber
     void run( boost::fibers::asio::yield_context yield)
     {
         std::string channel;
         try
         {
-            // read first message == channel name
             boost::system::error_code ec;
 
+            // read first message == channel name
+            // async_ready() returns if the the complete message is read
+            // until this the fiber is suspended until the complete message
+            // is read int the given buffer 'data'
             boost::asio::async_read(
                     socket_,
                     boost::asio::buffer( data_),
                     yield[ec]);
             if ( ec) throw std::runtime_error("no channel from subscriber");
+            // first message ist equal to the channel name the publisher
+            // publishes to
             channel = data_;
             
-            // register new channel
+            // subscribe to new channel
             reg_.subscribe( channel, shared_from_this() );
 
-            // wait for published messages
+            // read published messages
             for (;;)
             {
+                // wait for a conditon-variable for new messages
+                // the fiber will be suspended until the condtion
+                // becomes true and the fiber is resumed
+                // published message is stored in buffer 'data_'
                 boost::fibers::mutex::scoped_lock lk( mtx_);
                 cond_.wait( lk);
+                
+                // message '<fini>' terminates subscription
                 if ( "<fini>" == std::string( data_) ) break;
 
+                // async. write message to socket connected with
+                // subscriber
+                // async_write() returns if the complete message was writen
+                // the fiber is suspended in the meanwhile
                 boost::asio::async_write(
                         socket_,
                         boost::asio::buffer( data_, max_length),
@@ -221,7 +246,8 @@ subscriptions::publish( std::string const& msg)
     { s->publish( msg); }
 }
 
-
+// a publisher publishes messages on its channel
+// subscriber might register to this channel to get the published messages
 class publisher_session : public boost::enable_shared_from_this< publisher_session >
 {
 public:
@@ -233,37 +259,45 @@ public:
     tcp::socket& socket()
     { return socket_; }
 
+    // this function is executed inside the fiber
     void run( boost::fibers::asio::yield_context yield)
     {
         std::string channel;
         try
         {
+            boost::system::error_code ec;
+            
             // fixed size message
             char data[max_length];
 
             // read first message == channel name
-            boost::system::error_code ec;
+            // async_ready() returns if the the complete message is read
+            // until this the fiber is suspended until the complete message
+            // is read int the given buffer 'data'
             boost::asio::async_read(
                     socket_,
                     boost::asio::buffer( data),
                     yield[ec]);
             if ( ec) throw std::runtime_error("no channel from publisher");
+            // first message ist equal to the channel name the publisher
+            // publishes to
             channel = data;
             
-            // register new channel
+            // register the new channel
             reg_.register_channel( channel);
 
             // start publishing messages
             for (;;)
             {
-                // read message from publisher
+                // read message from publisher asyncronous
+                // async_read() suspends this fiber until the complete emssage is read
+                // and stored in the given buffer 'data'
                 boost::asio::async_read(
                         socket_,
                         boost::asio::buffer( data),
-                        yield[ec]); // does not work -> scheduler::run() not called
-                        //boost::fibers::asio::yield); 
+                        yield[ec]); 
                 if ( ec == boost::asio::error::eof)
-                    break; //connection closed cleanlyby peer
+                    break; //connection closed cleanly by peer
                 else if ( ec)
                     throw boost::system::system_error( ec); //some other error
 
@@ -285,44 +319,62 @@ private:
     registry        &   reg_;
 };
 
-
+// function accepts connections requests from clients acting as a publisher
 void accept_publisher( boost::asio::io_service& io_service,
                         unsigned short port,
                         registry & reg,
                         boost::fibers::asio::yield_context yield)
 {
+    // create TCP-acceptor
     tcp::acceptor acceptor( io_service, tcp::endpoint( tcp::v4(), port) );
 
+    // loop for accepting connection requests
     for (;;)
     {
         boost::system::error_code ec;
+        // create new publisher-session
+        // this instance will be associated with one publisher
         boost::shared_ptr< publisher_session > new_publisher_session( new publisher_session( io_service, reg) );
+        // async. accept of new connection request
+        // this function will suspend this execution context (fiber) until a
+        // connection was established, after returning from this function a new client (publisher)
+        // is connected
         acceptor.async_accept(
                 new_publisher_session->socket(), 
                 yield[ec]);
         if ( ! ec) {
+            // run the new publisher in its own fiber (one fiber for one client)
             boost::fibers::asio::spawn( io_service,
                 boost::bind( & publisher_session::run, new_publisher_session, _1) );
         }
     }
 }
 
-
+// function accepts connections requests from clients acting as a subscriber
 void accept_subscriber( boost::asio::io_service& io_service,
                         unsigned short port,
                         registry & reg,
                         boost::fibers::asio::yield_context yield)
 {
+    // create TCP-acceptor
     tcp::acceptor acceptor( io_service, tcp::endpoint( tcp::v4(), port) );
 
+    // loop for accepting connection requests
     for (;;)
     {
         boost::system::error_code ec;
+        // create new subscriber-session
+        // this instance will be associated with one subscriber
         boost::shared_ptr< subscriber_session > new_subscriber_session( new subscriber_session( io_service, reg) );
+        // async. accept of new connection request
+        // this function will suspend this execution context (fiber) until a
+        // connection was established, after returning from this function a new client (subscriber)
+        // is connected
         acceptor.async_accept(
                 new_subscriber_session->socket(), 
                 yield[ec]);
         if ( ! ec) {
+            // run the new subscriber in its own fiber (one fiber for one client)
             boost::fibers::asio::spawn( io_service,
                 boost::bind( & subscriber_session::run, new_subscriber_session, _1) );
         }
@@ -334,20 +386,25 @@ int main( int argc, char* argv[])
 {
     try
     {
+        // create io_service for async. I/O
         boost::asio::io_service io_service;
+        // install special fiber-scheduler dealing with asio's io_serivce
         boost::fibers::asio::round_robin ds( io_service);
         boost::fibers::set_scheduling_algorithm( & ds);
 
+        // registry for channels and its subscription
         registry reg;
 
+        // create an acceptor for publishers, run it as fiber
         boost::fibers::asio::spawn( io_service,
             boost::bind( accept_publisher,
                 boost::ref( io_service), 9997, boost::ref( reg), _1) );
 
+        // create an acceptor for subscribersm, run it as fiber
         boost::fibers::asio::spawn( io_service,
             boost::bind( accept_subscriber,
                 boost::ref( io_service), 9998, boost::ref( reg), _1) );
-
+                
         io_service.run();
     }
     catch ( std::exception const& e)
