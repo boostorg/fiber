89b3dafd5cad5ffaa7797466a801704ce942abf7
Nat Goodspeed <nat@lindenlab.com>
2015-05-11 09:48:23 -0400

    Eliminate spurious whitespace differences from upstream develop.

--- include/boost/fiber/algorithm.hpp
+++ include/boost/fiber/algorithm.hpp
@@ -10,13 +10,14 @@
 
 #include <boost/fiber/properties.hpp>
 #include <boost/fiber/detail/config.hpp>
- 
+
 #ifdef BOOST_HAS_ABI_HEADERS
 #  include BOOST_ABI_PREFIX
 #endif
 
 namespace boost {
 namespace fibers {
+
 class fiber_context;
 
 struct BOOST_FIBERS_DECL sched_algorithm {
