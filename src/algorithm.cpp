04be1f0f847177fd9014198846a0f66e0f287724
Nat Goodspeed <nat@lindenlab.com>
2015-05-13 08:41:38 -0400

    algorithm.hpp no longer lives in detail subdirectory.

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -4,7 +4,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include "boost/fiber/detail/algorithm.hpp"
+#include "boost/fiber/algorithm.hpp"
 
 #include <boost/config.hpp>
 
