0c18e79c14eebb9a5d5a405b2ba72d591ab5d195
Oliver Kowalke <oliver.kowalke@gmx.de>
2015-09-22 19:06:37 +0200

    add notice of derived work to tests

--- test/test_barrier.cpp
+++ test/test_barrier.cpp
@@ -3,6 +3,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
+//
+// This test is based on the tests of Boost.Thread
 
 #include <sstream>
 #include <string>
