253d981f52ebcafc9d698d6d896607a6f0d807f0
Oliver Kowalke <oliver.kowalke@gmx.de>
2015-09-29 17:55:46 +0200

    examples added

---
+++ examples/asio/daytime_client.cpp
@@ -0,0 +1,98 @@
+//
+// daytime_client.cpp
+// ~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// modified by Oliver Kowalke
+
+#include <iostream>
+
+#include <boost/array.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/udp.hpp>
+#include <boost/bind.hpp>
+#include <boost/ref.hpp>
+#include <boost/system/system_error.hpp>
+
+#include <boost/fiber/all.hpp>
+
+#include "loop.hpp"
+#include "use_future.hpp"
+
+using boost::asio::ip::udp;
+
+void get_daytime(boost::asio::io_service& io_service, const char* hostname)
+{
+  try
+  {
+    udp::resolver resolver(io_service);
+
+    boost::fibers::future<udp::resolver::iterator> iter =
+      resolver.async_resolve(
+          udp::resolver::query( udp::v4(), hostname, "daytime"),
+          boost::fibers::asio::use_future);
+
+    // The async_resolve operation above returns the endpoint iterator as a
+    // future value that is not retrieved ...
+
+    udp::socket socket(io_service, udp::v4());
+
+    boost::array<char, 1> send_buf  = {{ 0 }};
+    boost::fibers::future<std::size_t> send_length =
+      socket.async_send_to(boost::asio::buffer(send_buf),
+          *iter.get(), // ... until here. This call may block.
+          boost::fibers::asio::use_future);
+
+    // Do other things here while the send completes.
+
+    send_length.get(); // Blocks until the send is complete. Throws any errors.
+
+    boost::array<char, 128> recv_buf;
+    udp::endpoint sender_endpoint;
+    boost::fibers::future<std::size_t> recv_length =
+      socket.async_receive_from(
+          boost::asio::buffer(recv_buf),
+          sender_endpoint,
+          boost::fibers::asio::use_future);
+
+    // Do other things here while the receive completes.
+
+    std::cout.write(
+        recv_buf.data(),
+        recv_length.get()); // Blocks until receive is complete.
+  }
+  catch (boost::system::system_error& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+  io_service.stop();
+}
+
+int main( int argc, char* argv[])
+{
+    boost::asio::io_service io_service;
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: daytime_client <host>" << std::endl;
+            return 1;
+        }
+
+        boost::fibers::fiber(
+            boost::bind( get_daytime,
+                boost::ref( io_service), argv[1]) ).detach();
+        
+        boost::fibers::asio::run_service( io_service);
+    }
+    catch ( std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ examples/asio/promise_completion_token.hpp
@@ -0,0 +1,88 @@
+//
+// promise_completion_token.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// modified by Oliver Kowalke and Nat Goodspeed
+//
+
+#ifndef BOOST_FIBERS_ASIO_PROMISE_COMPLETION_TOKEN_HPP
+#define BOOST_FIBERS_ASIO_PROMISE_COMPLETION_TOKEN_HPP
+
+#include <boost/config.hpp>
+
+#ifdef BOOST_HAS_ABI_HEADERS
+#  include BOOST_ABI_PREFIX
+#endif
+
+namespace boost {
+namespace fibers {
+namespace asio {
+
+/// Common base class for yield_t and use_future_t. See also yield.hpp and
+/// use_future.hpp.
+/**
+ * The awkward name of this class is because it's not intended to be used
+ * directly in user code: it's the common base class for a couple of user-
+ * facing placeholder classes <tt>yield_t</tt> and <tt>use_future_t</tt>. They
+ * share a common handler class <tt>promise_handler</tt>.
+ *
+ * Each subclass (e.g. <tt>use_future_t</tt>) has a canonical instance
+ * (<tt>use_future</tt>). These may be used in the following ways as a
+ * Boost.Asio asynchronous operation completion token:
+ *
+ * <dl>
+ * <dt><tt>boost::fibers::asio::use_future</tt></dt>
+ * <dd>This is the canonical instance of <tt>use_future_t</tt>, provided
+ * solely for convenience. It causes <tt>promise_handler</tt> to allocate its
+ * internal <tt>boost::fibers::promise</tt> using a default-constructed
+ * default allocator (<tt>std::allocator<void></tt>).</dd>
+ * <dt><tt>boost::fibers::asio::use_future::with(alloc_instance)</tt></dt>
+ * <dd>This usage specifies an alternate allocator instance
+ * <tt>alloc_instance</tt>. It causes <tt>promise_handler</tt> to allocate its
+ * internal <tt>boost::fibers::promise</tt> using the specified
+ * allocator.</dd>
+ * </dl>
+ */
+//[fibers_asio_promise_completion_token
+template< typename Allocator >
+class promise_completion_token {
+public:
+    typedef Allocator allocator_type;
+
+    /// Construct using default-constructed allocator.
+    BOOST_CONSTEXPR promise_completion_token() :
+        ec_( nullptr) {
+    }
+
+    /// Construct using specified allocator.
+    explicit promise_completion_token( Allocator const& allocator) :
+        ec_( nullptr),
+        allocator_( allocator) {
+    }
+
+    /// Obtain allocator.
+    allocator_type get_allocator() const {
+        return allocator_;
+    }
+
+//private:
+    // used by some subclasses to bind an error_code to suppress exceptions
+    boost::system::error_code   *   ec_;
+
+private:
+    Allocator   allocator_;
+};
+//]
+
+}}}
+
+#ifdef BOOST_HAS_ABI_HEADERS
+#  include BOOST_ABI_SUFFIX
+#endif
+
+#endif // BOOST_FIBERS_ASIO_PROMISE_COMPLETION_TOKEN_HPP
