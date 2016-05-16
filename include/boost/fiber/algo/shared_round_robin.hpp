621de905f72f9fe7a3340e26657551c3e0c31488
Oliver Kowalke <oliver.kowalke@gmx.de>
2016-05-16 14:32:38 +0200

    update comment

--- include/boost/fiber/algo/shared_round_robin.hpp
+++ include/boost/fiber/algo/shared_round_robin.hpp
@@ -1,5 +1,5 @@
 
-//          Copyright Oliver Kowalke 2015.
+//          Copyright Nat Goodspeed + Oliver Kowalke 2015.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
