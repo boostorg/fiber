b5f278df341b9a39f76bf1cd7f756f2461fa70ab
Oliver Kowalke <oliver.kowalke@gmx.de>
2016-05-20 18:34:09 +0200

    use std::mutex for locking operations

--- include/boost/fiber/detail/spinlock.hpp
+++ include/boost/fiber/detail/spinlock.hpp
@@ -9,7 +9,6 @@
 #ifndef BOOST_FIBERS_SPINLOCK_H
 #define BOOST_FIBERS_SPINLOCK_H
 
-#include <atomic>
 #include <mutex>
 
 #include <boost/fiber/detail/config.hpp>
@@ -17,25 +16,6 @@
 namespace boost {
 namespace fibers {
 namespace detail {
-
-class BOOST_FIBERS_DECL atomic_spinlock {
-private:
-    enum class spinlock_status {
-        locked = 0,
-        unlocked
-    };
-
-    std::atomic< spinlock_status >  state_{ spinlock_status::unlocked };
-
-public:
-    atomic_spinlock() noexcept = default;
-
-    atomic_spinlock( atomic_spinlock const&) = delete;
-    atomic_spinlock & operator=( atomic_spinlock const&) = delete;
-
-    void lock() noexcept;
-    void unlock() noexcept;
-};
 
 struct non_spinlock {
     constexpr non_spinlock() noexcept {}
@@ -49,8 +29,8 @@ struct non_lock {
     void unlock() noexcept {}
 };
 
-#if ! defined(BOOST_FIBES_NO_ATOMICS) 
-typedef atomic_spinlock spinlock;
+#if ! defined(BOOST_FIBERS_NO_ATOMICS) 
+typedef std::mutex      spinlock;
 using spinlock_lock = std::unique_lock< spinlock >;
 #else
 typedef non_spinlock    spinlock;
