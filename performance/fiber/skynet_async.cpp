dec1701e30573a33b23fccc759b23c60a50dfb32
Brandon Kohn <blkohn@hotmail.com>
2017-01-12 09:23:32 -0500

    Updated formatting to match existing style.

--- performance/fiber/skynet_async.cpp
+++ performance/fiber/skynet_async.cpp
@@ -40,15 +40,13 @@ static boost::fibers::condition_variable_any cnd{};
 // microbenchmark
 std::uint64_t skynet(allocator_type& salloc, std::uint64_t num, std::uint64_t size, std::uint64_t div)
 {
-    if (size != 1)
-    {
+    if ( size != 1){
         size /= div;
 
         std::vector<boost::fibers::future<std::uint64_t> > results;
-        results.reserve(div);
+        results.reserve( div);
 
-        for (std::uint64_t i = 0; i != div; ++i)
-        {
+        for ( std::uint64_t i = 0; i != div; ++i){
             std::uint64_t sub_num = num + i * size;
             results.emplace_back(boost::fibers::async(
                   boost::fibers::launch::dispatch
@@ -58,7 +56,7 @@ std::uint64_t skynet(allocator_type& salloc, std::uint64_t num, std::uint64_t si
         }
 
         std::uint64_t sum = 0;
-        for (auto & f : results)
+        for ( auto& f : results)
             sum += f.get();
         return sum;
     }
@@ -66,26 +64,26 @@ std::uint64_t skynet(allocator_type& salloc, std::uint64_t num, std::uint64_t si
 }
 
 void thread( unsigned int max_idx, unsigned int idx, barrier * b) {
-    bind_to_processor(idx);
-    boost::fibers::use_scheduling_algorithm< boost::fibers::algo::work_stealing >(max_idx, idx);
+    bind_to_processor( idx);
+    boost::fibers::use_scheduling_algorithm< boost::fibers::algo::work_stealing >( max_idx, idx);
     b->wait();
-    lock_type lk(mtx);
+    lock_type lk( mtx);
     cnd.wait(lk, [](){ return done; });
-    BOOST_ASSERT(done);
+    BOOST_ASSERT( done);
 }
 
 int main() {
     try {
         unsigned int cpus = std::thread::hardware_concurrency();
-        barrier b(cpus);
+        barrier b( cpus);
         unsigned int max_idx = cpus - 1;
-        boost::fibers::use_scheduling_algorithm< boost::fibers::algo::work_stealing >(max_idx, max_idx);
-        bind_to_processor(max_idx);
+        boost::fibers::use_scheduling_algorithm< boost::fibers::algo::work_stealing >( max_idx, max_idx);
+        bind_to_processor( max_idx);
         std::size_t stack_size{ 4048 };
         std::size_t size{ 100000 };//! NOTE: This can handle the standard 1 million.
         std::size_t div{ 10 };
         std::vector< std::thread > threads;
-        for (unsigned int idx = 0; idx < max_idx; ++idx) {
+        for ( unsigned int idx = 0; idx < max_idx; ++idx) {
             threads.push_back( std::thread( thread, max_idx, idx, & b) );
         };
         allocator_type salloc{ stack_size };
@@ -93,14 +91,14 @@ int main() {
         duration_type duration{ duration_type::zero() };
         b.wait();
         time_point_type start{ clock_type::now() };
-        result = skynet(salloc, 0, size, div);
+        result = skynet( salloc, 0, size, div);
         duration = clock_type::now() - start;
         std::cout << "Result: " << result << " in " << duration.count() / 1000000 << " ms" << std::endl;
-        lock_type lk(mtx);
+        lock_type lk( mtx);
         done = true;
         lk.unlock();
         cnd.notify_all();
-        for (std::thread & t : threads) {
+        for ( std::thread & t : threads) {
             t.join();
         }
         std::cout << "done." << std::endl;
