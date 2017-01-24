2f3ca28af7ee28295a050522ba909c1c1e868480
oliver Kowalke <oliver.kowalke@gmail.com>
2017-01-24 13:54:20 +0100

    modify example ping-pong

--- examples/ping_pong.cpp
+++ examples/ping_pong.cpp
@@ -5,34 +5,30 @@
 
 #include <boost/fiber/all.hpp>
 
-typedef boost::fibers::buffered_channel< std::string >	channel_t;
+int main() {
+    using channel_t = boost::fibers::buffered_channel< std::string >;
+	try {
+        channel_t chan1{ 1 }, chan2{ 1 };
 
-inline
-void foo( channel_t & chan1, channel_t & chan2, std::string const& msg)
-{
-    boost::fibers::fiber::id id( boost::this_fiber::get_id() );
-	chan1.push( msg);
-    std::string value{ chan2.value_pop() };
-    std::cout << "fiber " <<  id << ": " <<  value << " received" << std::endl;
-	chan1.push( msg);
-    value = chan2.value_pop();
-    std::cout << "fiber " <<  id << ": " << value << " received" << std::endl;
-	chan1.push( msg);
-    value = chan2.value_pop();
-    std::cout << "fiber " <<  id << ": " << value << " received" << std::endl;
-}
+        boost::fibers::fiber fping([&chan1,&chan2]{
+                    chan1.push( "ping");
+                    std::cout << chan2.value_pop() << "\n";
+                    chan1.push( "ping");
+                    std::cout << chan2.value_pop() << "\n";
+                    chan1.push( "ping");
+                    std::cout << chan2.value_pop() << "\n";
+                });
+        boost::fibers::fiber fpong([&chan1,&chan2]{
+                    std::cout << chan1.value_pop() << "\n";
+                    chan2.push( "pong");
+                    std::cout << chan1.value_pop() << "\n";
+                    chan2.push( "pong");
+                    std::cout << chan1.value_pop() << "\n";
+                    chan2.push( "pong");
+                });
 
-int main()
-{
-	try
-	{
-        channel_t chan1{ 2 }, chan2{ 2 };
-
-        boost::fibers::fiber f1( & foo, std::ref( chan1), std::ref( chan2), std::string("ping") );
-        boost::fibers::fiber f2( & foo, std::ref( chan2), std::ref( chan1), std::string("pong") );
-
-        f1.join();
-        f2.join();
+        fping.join();
+        fpong.join();
 
 		std::cout << "done." << std::endl;
 
