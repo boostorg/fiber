de34f08c753596c6ce465b34162f628e096281ae
oliver Kowalke <oliver.kowalke@gmail.com>
2017-03-24 08:51:22 +0100

    reorder member vars for mutex types

--- include/boost/fiber/timed_mutex.hpp
+++ include/boost/fiber/timed_mutex.hpp
@@ -37,9 +37,9 @@ private:
 
     typedef context::wait_queue_t   wait_queue_type;
 
-    context                 *   owner_{ nullptr };
-    wait_queue_type             wait_queue_{};
     detail::spinlock            wait_queue_splk_{};
+    wait_queue_type             wait_queue_{};
+    context                 *   owner_{ nullptr };
 
     bool try_lock_until_( std::chrono::steady_clock::time_point const& timeout_time) noexcept;
 
