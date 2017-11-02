5fa2d03caad5eba7f1cca53d62992c242327edd3
Oliver Kowalke <oliver.kowalke@gmail.com>
2017-11-02 21:50:47 +0100

    update license for compute examples

--- examples/hip/multiple_streams.cpp
+++ examples/hip/multiple_streams.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * NVIDIA Corporation and its licensors retain all intellectual property and 
+ * proprietary rights in and to this software and related documentation. 
+ * Any use, reproduction, disclosure, or distribution of this software 
+ * and related documentation without an express license agreement from
+ * NVIDIA Corporation is strictly prohibited.
+ *
+ * Please refer to the applicable NVIDIA end user license agreement (EULA) 
+ * associated with this source code for terms and conditions that govern 
+ * your use of this NVIDIA software.
+ * 
+ */
+
 #include "hip/hip_runtime.h"
 #include <chrono>
 #include <cstdlib>
