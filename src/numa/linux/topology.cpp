a19deebe229ada8dc6d94e7300df766095af33e0
Peter Dimov <pdimov@pdimov.com>
2017-11-08 15:54:15 +0200

    Disable topology on Linux w/o <regex>

--- src/numa/linux/topology.cpp
+++ src/numa/linux/topology.cpp
@@ -5,6 +5,7 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #include "boost/fiber/numa/topology.hpp"
+#include "boost/fiber/exceptions.hpp"
 
 #include <exception>
 #include <map>
@@ -23,6 +24,8 @@
 #ifdef BOOST_HAS_ABI_HEADERS
 # include BOOST_ABI_PREFIX
 #endif
+
+#if !defined(BOOST_NO_CXX11_HDR_REGEX)
 
 namespace al = boost::algorithm;
 namespace fs = boost::filesystem;
@@ -189,6 +192,31 @@ std::vector< node > topology() {
 }
 
 }}}
+
+#else
+
+namespace boost {
+namespace fibers {
+namespace numa {
+
+#if BOOST_COMP_CLANG || \
+    BOOST_COMP_GNUC || \
+    BOOST_COMP_INTEL ||  \
+    BOOST_COMP_MSVC
+# pragma message "topology() not supported without <regex>"
+#endif
+
+BOOST_FIBERS_DECL
+std::vector< node > topology() {
+    throw fiber_error{
+        std::make_error_code( std::errc::function_not_supported),
+            "boost fiber: topology() not supported without <regex>" };
+    return std::vector< node >{};
+}
+
+}}}
+
+#endif
 
 #ifdef BOOST_HAS_ABI_HEADERS
 # include BOOST_ABI_SUFFIX
