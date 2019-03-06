9644743c372f92aebffb8c992766b15e45a8e845
Roman Gershman <romange@gmail.com>
2019-03-06 12:07:10 +0200

    Fix https://github.com/boostorg/fiber/issues/194

--- src/condition_variable.cpp
+++ src/condition_variable.cpp
@@ -15,6 +15,13 @@
 namespace boost {
 namespace fibers {
 
+// We switch in case it's a timed-wait or a regular wait.
+inline bool should_switch(fibers::context* ctx, std::intptr_t expected_tw) {
+    return ctx->twstatus.compare_exchange_strong(expected_tw, static_cast<std::intptr_t>(-1),
+                                                 std::memory_order_acq_rel) ||
+         expected_tw == 0;
+}
+
 void
 condition_variable_any::notify_one() noexcept {
     context * active_ctx = context::active();
@@ -23,13 +30,8 @@ condition_variable_any::notify_one() noexcept {
     while ( ! wait_queue_.empty() ) {
         context * ctx = & wait_queue_.front();
         wait_queue_.pop_front();
-        std::intptr_t expected = reinterpret_cast< std::intptr_t >( this);
-        if ( ctx->twstatus.compare_exchange_strong( expected, static_cast< std::intptr_t >( -1), std::memory_order_acq_rel) ) {
-            // notify context
-            active_ctx->schedule( ctx);
-            break;
-        } else if ( static_cast< std::intptr_t >( 0) == expected) {
-            // no timed-wait op.
+        if (should_switch(ctx, reinterpret_cast< std::intptr_t >(this))) {
+            lk.unlock();  // We can not access any data member after active_ctx->schedule() called.
             // notify context
             active_ctx->schedule( ctx);
             break;
@@ -40,18 +42,19 @@ condition_variable_any::notify_one() noexcept {
 void
 condition_variable_any::notify_all() noexcept {
     context * active_ctx = context::active();
-    // get all context' from wait-queue
+
+    // get all context' from wait-queue. We move the wait list into a temporary structure
+    // on stack to minimize the contention on the spinlock and to prevent the scenario
+    // were "this" object is destroyed but the lock still holds its spinlock member.
     detail::spinlock_lock lk{ wait_queue_splk_ };
+    wait_queue_t tmp{std::move(wait_queue_)};
+    lk.unlock();
+
     // notify all context'
-    while ( ! wait_queue_.empty() ) {
-        context * ctx = & wait_queue_.front();
-        wait_queue_.pop_front();
-        std::intptr_t expected = reinterpret_cast< std::intptr_t >( this);
-        if ( ctx->twstatus.compare_exchange_strong( expected, static_cast< std::intptr_t >( -1), std::memory_order_acq_rel) ) {
-            // notify context
-            active_ctx->schedule( ctx);
-        } else if ( static_cast< std::intptr_t >( 0) == expected) {
-            // no timed-wait op.
+    while ( ! tmp.empty() ) {
+        context * ctx = & tmp.front();
+        tmp.pop_front();
+        if (should_switch(ctx, reinterpret_cast< std::intptr_t >( this))) {
             // notify context
             active_ctx->schedule( ctx);
         }
